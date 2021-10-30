f35dfbb6083a60f61f719fd4d02fd9653815ac4a
RafaelLyrioMacias <75077935+RafaelLyrioMacias@users.noreply.github.com>
2021-10-30 10:07:01 -0300

    Add files via upload

---
+++ src/pages/Courses/index.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { PageHeaderLogin } from '../../components/PageHeaderBootstrap';
+export default class Courses extends Component {
+    render() {
+        return (
+            <div className="content">
+                <p>Tela Cursos</p>
+                <PageHeaderLogin />
+            </div>
+        )     
+    }
+}
